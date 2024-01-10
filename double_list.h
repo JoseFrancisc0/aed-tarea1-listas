b2ac3dc1d17634a16e2ac8e2918b3d0c6db4a3d1
JoseFrancisc0 <jose.wong@utec.edu.pe>
2024-01-10 16:13:21 -0500

    Esqueleto de las listas

--- double_list.h
+++ double_list.h
@@ -1,6 +1,32 @@
 #ifndef TAREA1_LISTAS_DOUBLY_LINKED_LIST_H
 #define TAREA1_LISTAS_DOUBLY_LINKED_LIST_H
 
-class Doubly_Linked_List{};
+template<typename T>
+class Doubly_Linked_List{
+    private:
+        struct Nodo{
+            T data;
+            Nodo* prev;
+            Nodo* next;
+        };
+    public:
+        Doubly_Linked_List();
+
+        T front();
+        T back();
+        void push_front(T _data);
+        void push_back(T _data);
+        T pop_front();
+        T pop_back();
+        void insert(T _data, int index);
+        void erase(T _data);
+        T operator[](int index);
+        bool empty();
+        int size();
+        void clear();
+        void reverse();
+
+        ~Doubly_Linked_List();
+};
 
 #endif //TAREA1_LISTAS_DOUBLY_LINKED_LIST_H
--- forward_list.h
+++ forward_list.h
@@ -1,6 +1,30 @@
 #ifndef TAREA1_LISTAS_FORWARD_LIST_H
 #define TAREA1_LISTAS_FORWARD_LIST_H
 
-class Forward_List{};
+template<typename T>
+class Forward_List{
+    private:
+        struct Nodo{
+            T data;
+            Nodo* next;
+        }
+    public:
+        Forward_List();
+
+        T front();
+        T back();
+        void push_front(T _data);
+        void push_back(T _data);
+        T pop_front();
+        T pop_back();
+        T operator[](int index);
+        bool empty();
+        int size();
+        void clear();
+        void sort();
+        void reverse();
+
+        ~Forward_List();
+};
 
 #endif //TAREA1_LISTAS_FORWARD_LIST_H
